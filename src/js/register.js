f1594c7f5bd21b8b60c0af67a95d7f5f0a3b670c
Baolo <524172671@qq.com>
2016-10-13 14:00:31 +0800

    修改了并完善了注册页面，改了通过验证登录，还有就是首页右边被撑大，宽的100%，和不给的区别

--- src/js/register.js
+++ src/js/register.js
@@ -1,5 +1,12 @@
 
 $(function(){
+	
+	//设置全局的变量标记；
+	var flag1 = false;//标记用户；
+	var flag2 = false;//标记密码；
+	var flag3 = false;//标记重写密码；
+	var flag4 = false;//标记阅读条文；
+	var flag5 = false;//标记验证码；
 	//用户名验证；
 	$(".login-tel").on("blur",function(){
 		registernNameTake();
@@ -51,6 +58,7 @@ function registernNameTake(){
 	}else if(/^1(3|5|8|4|7)[0-9]{9}$/.test($this.val())){
 		$this.css("border-color","#bfbdbf");
 		$this.parent().next().css("visibility","hidden");
+		flag1 = true;
 		return false;
 	}else{
 		$this.css("border-color","#f20266");
@@ -70,6 +78,7 @@ function registernNameTake(){
 		else if(/[a-zA-Z0-9]{6,20}/.test($this.val())){
 			$this.css("border-color","#bfbdbf");
 			$this.parent().next().css("visibility","hidden");
+			flag2 = true;
 			return false;
 		}
 		else{
@@ -90,6 +99,7 @@ function registernNameTake(){
 		else if($this.val() == $pswVal){
 			$this.css("border-color","#bfbdbf");
 			$this.parent().next().css("visibility","hidden");
+			flag3 = true;
 			return false;
 		}else{
 			$this.css("border-color","#f20266");
@@ -102,10 +112,79 @@ function registernNameTake(){
 		var $read = $(".checkbox-active");
 		if($read.is(":checked")){
 			$read.parent().next().css("visibility","hidden");
+			flag4 = true;
 			return true;
 		}else{
 			$read.parent().next().css("visibility","visible");
 			return false;
 		}
-	}
+	};
+	
+	//插件验证码
+	//js验证码；
+	var inpTake = document.getElementById("pcode");
+	var tiShi = document.getElementById("yantishi");
+	var code = document.getElementById("verifyimage");
+	var code1 = document.getElementById("huanyizhang");
+	//调用函数；
+	angin();
+
+	function angin() {
+		var c = new KinerCode({
+			len: 4, //验证码长度；
+			chars: [
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+			], //指定验证码词典，提供搜索；
+			question: false, //经典模式选择false；
+			copy: false, //是否允许复制；
+			bgColor: "", //背景色【与背景图任选其一】
+			bgImg: "../img/reg/yanzheng.jpg", //背景图，背景色会失效
+			randomBg: false,
+			inputArea: inpTake, //输入验证码的input对象绑定，
+			codeArea: code, //验证码放置的区域【HTMLDivElement 】
+			click2refresh: true, //是否点击验证码刷新验证码
+			false2refresh: true, //在填错验证码后是否刷新验证码
+			validateEven: "blur", //触发验证的方法名，如click，blur等
+			validateFn: function(result, code) { //验证回调函数
+				if(result) {
+					tiShi.style.display = "none";
+					flag5 = true;
+				} else {
+					tiShi.style.display = "block";
+				}
+			}
+		})
+	};
+
+			//点注册按钮生成cookies信息；当所有标记都为ture是生成cookies并进行跳转到登录页面；
+			$(".login-sprite-btn").on("click", function() {
+				registernNameTake();
+				registerPswTake();
+				registerPswAgTake();
+				registerReadTake();
+				angin();
+			var $usePhone = $(".login-tel").val();
+			var $psw = $("#pass3").val();
+			var $pswAg = $("#pass4").val();
+			if(flag1 && flag2 && flag3 && flag4 && flag5){
+				//设置cookie
+				var oList = [];
+				oList.push({
+				user: $usePhone,
+				psw: $psw
+				});
+				var userPass = JSON.stringify(oList);
+				$.cookie("userpass", userPass, {expries: 7}, {path: "/"});
+				alert("注册成功，请移步到登录页面登录");
+				location.href = "login.html";
+			}else{
+				alert("填写信息有误");
+			}
+			//阻止submit提交；
+			return false;
+		});
+		
+		
 })
